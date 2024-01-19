144efd6312cde4fecf395baf453660b2d8b33ca6
Luis Reyes <yuiftw05@gmail.com>
2024-01-19 04:16:09 -0600

    Button: updating the button styles if the prop theme is true

--- components/Button.js
+++ components/Button.js
@@ -1,7 +1,31 @@
 import { StyleSheet, View, Pressable, Text } from "react-native";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
 
-const Button = ({ label }) => {
-  return (
+const Button = ({ label, theme }) => {
+
+    if(theme === 'primary'){
+        return(
+            <View style={[styles.buttonContainer, {borderWidth: 4, borderColor: '#ffd33d', borderRadius: 18}]}>
+                <Pressable 
+                style={[styles.button, {backgroundColor: '#fff'}]}
+                onPress={()=> alert('button pressed')}
+                >
+                    <FontAwesome
+                    name="picture-o"
+                    size={18}
+                    color='#25292e'
+                    style={styles.buttonIcon}
+                    />
+                    <Text style={[styles.buttonLabel, {color: '#25292e'}]}>{label}</Text>
+                </Pressable>
+            </View>
+        )
+    }
+  
+  
+  
+  
+    return (
     <View style={styles.buttonContainer}>
       <Pressable style={styles.button} onPress={() => alert("button pressed")}>
         <Text style={styles.buttonLabel}>{label}</Text>
@@ -26,7 +50,7 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
     flexDirection: "row",
-    backgroundColor: '#000',
+    backgroundColor: 'blue',
   },
   buttonIcon: {
     paddingRight: 8,
